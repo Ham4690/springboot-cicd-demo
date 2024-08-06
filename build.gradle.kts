6e9628d7641ca263aa041e66a167149ff6b51228
Arata Higashiguchi <ham46903@gmail.com>
2024-08-07 00:49:53 +0900

    null安全のためのライブラリを導入

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import net.ltgt.gradle.errorprone.CheckSeverity
+import net.ltgt.gradle.errorprone.errorprone
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("net.ltgt.errorprone") version "4.0.1"
 }
 
 group = "com.example"
@@ -19,10 +23,27 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// NullAway Setting
+	errorprone("com.uber.nullaway:nullaway:0.11.1")
+	compileOnly("org.jspecify:jspecify:1.0.0")
+	errorprone("com.google.errorprone:error_prone_core:2.29.2")
 }
+
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+	// テストコードに対してもNullAwayを実行したい場合はif条件を削除します
+	if (!name.lowercase().contains("test")) {
+		options.errorprone {
+			check("NullAway", CheckSeverity.ERROR)
+			option("NullAway:AnnotatedPackages", "com.example") // 解析対象のパッケージ名を適宜変更してください
+		}
+	}
 }
